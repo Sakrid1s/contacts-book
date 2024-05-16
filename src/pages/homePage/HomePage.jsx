8548ad213c1bba6ce4028fa54a7214e603b4fb7f
Sakrid1s <bilinets.vitalik@gmail.com>
2024-05-16 18:50:23 +0300

    upd

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,21 +1,39 @@
-// import Container from '../../components/container/Container';
 import DocumentTitle from '../../components/documentTitle/DocumentTitle';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
     <>
       <DocumentTitle>Home</DocumentTitle>
-      {/* <Container> */}
       <div className={css.HomePageContainer}>
         <h1 className={css.title}>
-          Task manager welcome page{' '}
+          Contacts book welcome page{' '}
           <span role="img" aria-label="Greeting icon">
             💁‍♀️
           </span>
         </h1>
+        <div className={css.linksContainer}>
+          <a
+            href="https://github.com/Sakrid1s"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className={css.linkWrapper}>
+              <FaGithub className={css.icon} />
+            </div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/vitalii-bilinets/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className={css.linkWrapper}>
+              <FaLinkedin className={css.icon} />
+            </div>
+          </a>
+        </div>
       </div>
-      {/* </Container> */}
     </>
   );
 };
