fe54a6f359a7a49f0831b81f0133310e27d440be
Sakrid1s <bilinets.vitalik@gmail.com>
2024-05-25 01:56:05 +0300

    upd

--- src/components/authNav/AuthNav.jsx
+++ src/components/authNav/AuthNav.jsx
@@ -1,14 +1,18 @@
 import { NavLink } from 'react-router-dom';
-
+import clsx from 'clsx';
 import css from './AuthNav.module.css';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 const AuthNav = () => {
   return (
     <div>
-      <NavLink className={css.link} to="/register">
+      <NavLink className={buildLinkClass} to="/register">
         Register
       </NavLink>
-      <NavLink className={css.link} to="/login">
+      <NavLink className={buildLinkClass} to="/login">
         Log In
       </NavLink>
     </div>
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,19 +1,23 @@
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { authSelectors } from '../../redux/auth/selectors';
-
+import clsx from 'clsx';
 import css from './Navigation.module.css';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 const Navigation = () => {
   const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
 
   return (
     <nav>
-      <NavLink className={css.link} to="/">
+      <NavLink className={buildLinkClass} to="/">
         Home
       </NavLink>
       {isLoggedIn && (
-        <NavLink className={css.link} to="/contacts">
+        <NavLink className={buildLinkClass} to="/contacts">
           Contacts
         </NavLink>
       )}
